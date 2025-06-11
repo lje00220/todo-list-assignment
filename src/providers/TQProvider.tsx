3cb288630a16cc8e786bf94048648b333c241282
lje00220 <155710708+lje00220@users.noreply.github.com>
2025-06-11 14:09:25 +0900

    feature:#5 tanstack query provider 설정

---
+++ src/providers/TQProvider.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { useState } from 'react';
+
+const TQProvider = ({ children }: { children: React.ReactNode }) => {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 60 * 1000,
+          },
+        },
+      }),
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
+      {children}
+    </QueryClientProvider>
+  );
+};
+
+export default TQProvider;
