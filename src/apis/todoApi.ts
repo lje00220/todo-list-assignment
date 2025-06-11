6245b5bbef77365eb52201f1591aabdfbca65e4c
lje00220 <155710708+lje00220@users.noreply.github.com>
2025-06-11 16:08:30 +0900

    feature:#5 todo 생성 API 함수 추가

--- src/apis/todoApi.ts
+++ src/apis/todoApi.ts
@@ -5,7 +5,19 @@ const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
+/**
+ * GET 요청을 통해 todo 리스트를 가져오는 함수
+ * @returns Promise<TodoType[]> - todo 리스트
+ */
 export const getTodos = async (): Promise<TodoType[]> => {
   const response = await api.get('/todos');
   return response.data;
+};
+
+/**
+ * POST 요청을 통해 새로운 todo를 추가하는 함수
+ * @param todo - id를 제외한 todo 객체
+ */
+export const postTodos = async (todo: Omit<TodoType, 'id'>) => {
+  await api.post('/todos', todo);
 };
