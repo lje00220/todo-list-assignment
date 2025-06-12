13409bc09008fe329c9202f87bc8438c25ecceb0
lje00220 <155710708+lje00220@users.noreply.github.com>
2025-06-12 11:58:11 +0900

    refactor:#23 컨테이너 컴포넌트 분리

--- src/components/TodoListClient.tsx
+++ src/components/TodoListClient.tsx
@@ -2,6 +2,7 @@
 
 import { useTodoQuery } from '@/hooks/useTodoQuery';
 import { useState } from 'react';
+import Container from './Container';
 import TodoInputForm from './TodoInputForm';
 import Dropdown from './Dropdown';
 import TodoItem from './TodoItem';
@@ -32,30 +33,28 @@ const TodoListClient = () => {
   });
 
   return (
-    <main className="flex h-screen items-center justify-center bg-gray-100">
-      <div className="flex h-[80vh] w-full flex-col rounded-lg bg-white py-10 shadow-xl sm:h-[70vh] sm:w-2/3 md:w-3/5 lg:w-1/2">
-        {/* 상단 바 영역 */}
-        <header className="px-6 py-8">
-          <h1 className="mb-6 text-center text-3xl font-bold">투두리스트</h1>
-          <TodoInputForm />
-        </header>
+    <Container>
+      {/* 상단 바 영역 */}
+      <header className="px-6 py-8">
+        <h1 className="mb-6 text-center text-3xl font-bold">투두리스트</h1>
+        <TodoInputForm />
+      </header>
 
-        <hr className="border-b border-gray-200" />
+      <hr className="border-b border-gray-200" />
 
-        {/* 리스트 영역 */}
-        <section className="flex h-full flex-col gap-3 overflow-y-auto px-4 py-6 sm:px-8">
-          <div className="flex justify-end">
-            <Dropdown setFilteredOption={setFilteredOption} />
-          </div>
+      {/* 리스트 영역 */}
+      <section className="flex h-full flex-col gap-3 overflow-y-auto px-4 py-6 sm:px-8">
+        <div className="flex justify-end">
+          <Dropdown setFilteredOption={setFilteredOption} />
+        </div>
 
-          {isLoading ? (
-            <Loading />
-          ) : (
-            todos?.map((todo) => <TodoItem key={todo.id} todo={todo} />)
-          )}
-        </section>
-      </div>
-    </main>
+        {isLoading ? (
+          <Loading />
+        ) : (
+          todos?.map((todo) => <TodoItem key={todo.id} todo={todo} />)
+        )}
+      </section>
+    </Container>
   );
 };
 
